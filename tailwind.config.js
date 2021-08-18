33cc9c62bd4a74a07a1c53132d418460ef26b802
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-18 13:39:06 +0700

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-module.exports = {
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const config = {
     purge: ['./src/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     variants: {
         extend: {},
     },
     plugins: [],
 }
+
+module.exports = config
