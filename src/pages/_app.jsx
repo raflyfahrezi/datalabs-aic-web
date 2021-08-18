8abb3cd8007750d14bf2fbbec3c084fa9465acbd
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-18 13:37:49 +0700

    importing open sans

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import 'tailwindcss/tailwind.css'
+import '@fontsource/open-sans/400.css'
 
 const MyApp = ({ Component, pageProps }) => {
     return <Component {...pageProps} />
