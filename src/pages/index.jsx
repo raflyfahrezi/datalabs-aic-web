761687728eb3ac5d133e19f54268503cfa317f3d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-18 13:41:30 +0700

    layouting index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const index = () => {
     return (
-        <div>
-            <p>hello world</p>
+        <div className='text-center pt-10'>
+            <p className='text-2xl font-bold capitalize'>hello world</p>
         </div>
     )
 }
