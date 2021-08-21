81b30a31029793c8289a0b65e2b5ac8fe2c606ac
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-21 11:30:05 +0700

    change paragraph base color

--- src/typography/paragraph/index.jsx
+++ src/typography/paragraph/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const paragraph = ({ className, children }) => {
-    return <p className={`text-base ${className}`}>{children}</p>
+    return <p className={`text-base text-white ${className}`}>{children}</p>
 }
 
 export default paragraph
