4b1376740cbe1d4fe9be27b5a390128384ac44bf
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-21 13:13:45 +0700

    edit textField style

--- src/components/textField/index.jsx
+++ src/components/textField/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const textField = ({ className, ...props }) => {
-    return <input type='text' className={`p-4 ${className}`} {...props} />
+    return (
+        <input
+            type='text'
+            className={`px-3 py-3 outline-none bg-gray-800 text-white rounded ${className}`}
+            {...props}
+        />
+    )
 }
 
 export default textField
