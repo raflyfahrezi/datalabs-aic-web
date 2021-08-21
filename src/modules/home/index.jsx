5d9ea3cee5007ee33bce2ca38dc2df5a31b43995
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-21 12:41:27 +0700

    implement component wrapper on home modulel

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
+import { ComponentWrapper } from '@components'
 import { Heading1, Paragraph } from '@typography'
 
 const home = () => {
     const appDescription = process.env.NEXT_PUBLIC_APP_DESCRIPTION
 
     return (
-        <div>
-            <div>
-                <Heading1>Learn Something New Here</Heading1>
+        <ComponentWrapper>
+            <div className='w-full h-60 grid place-items-center'>
+                <div>
+                    <Heading1>Learn Something New Here</Heading1>
+                    <Paragraph className='text-gray-400'>
+                        {appDescription}
+                    </Paragraph>
+                </div>
             </div>
-            <Paragraph className='text-gray-400'>{appDescription}</Paragraph>
-        </div>
+        </ComponentWrapper>
     )
 }
 
