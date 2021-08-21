9829b95ab480cc1ef333c7d491a888f8b714e82c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-21 12:43:17 +0700

    edit componentWrapper style

--- src/components/componentWrapper/index.jsx
+++ src/components/componentWrapper/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const componentWrapper = ({ children }) => {
-    return <div className='w-full mx-2'>{children}</div>
+    return <div className='w-full max-w-screen-lg mx-auto px-2'>{children}</div>
 }
 
 export default componentWrapper
