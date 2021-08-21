affe405f15d5c93672b5bd364b07269e4a344e27
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-21 11:22:32 +0700

    create link component and exporting it

---
+++ src/components/link/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+const link = ({ href, children, external }) => {
+    if (external) {
+        return (
+            <a href={href} rel='noreferrer noopener'>
+                {children}
+            </a>
+        )
+    }
+
+    return (
+        <Link href={href}>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a>{children}</a>
+        </Link>
+    )
+}
+
+export default link
