c104f3f05605ab5a0fd92612b025264352217d23
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-21 12:38:18 +0700

    create component wrapper and exporting it

---
+++ src/components/componentWrapper/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const componentWrapper = ({ children }) => {
+    return <div className='w-full mx-2'>{children}</div>
+}
+
+export default componentWrapper
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 import Link from './link'
+import ComponentWrapper from './componentWrapper'
 
-export { Link }
+export { Link, ComponentWrapper }
