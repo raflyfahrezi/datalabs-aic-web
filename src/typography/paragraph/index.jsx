c4633a0b6810e9b04491753ebd2a77b020f40a78
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-22 16:11:54 +0700

    adding secondary props in paragraph

--- src/typography/paragraph/index.jsx
+++ src/typography/paragraph/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const paragraph = ({ className, children }) => {
-    return <p className={`text-base text-white ${className}`}>{children}</p>
+const paragraph = ({ children, className, secondary }) => {
+    return (
+        <p
+            className={`text-base text-white ${
+                secondary && 'text-gray-400'
+            } ${className}`}
+        >
+            {children}
+        </p>
+    )
 }
 
 export default paragraph
