5c394aca5f1aea0657f1242733ddc2f5570598ff
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-22 16:39:42 +0700

    fix paragraph className

--- src/typography/paragraph/index.jsx
+++ src/typography/paragraph/index.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const paragraph = ({ children, className, secondary }) => {
     return (
         <p
-            className={`text-base text-white ${
-                secondary && 'text-gray-400'
+            className={`text-white ${
+                secondary ? 'text-gray-400' : ''
             } ${className}`}
         >
             {children}
