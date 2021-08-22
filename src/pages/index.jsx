7c9b7c15ab6efc6ced4001d4f3dbaf5a1ae6f340
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-22 15:47:10 +0700

    fetcing coursesList data on home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,22 @@ import React from 'react'
 import { Home } from '@modules'
 import { PageWrapper } from '@layout'
 
-const index = () => {
+const index = ({ coursesList }) => {
     return (
         <PageWrapper title='Home'>
-            <Home />
+            <Home coursesList={coursesList} />
         </PageWrapper>
     )
 }
 
+const getStaticProps = async () => {
+    import { coursesList } from '@assets'
+
+    return {
+        props: { coursesList },
+        revalidate: 1000,
+    }
+}
+
 export default index
+export { getStaticProps }
