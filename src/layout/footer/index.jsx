0483d8e3832c294ae50919c8db668161a2cc303e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-22 17:07:23 +0700

    fix footer

--- src/layout/footer/index.jsx
+++ src/layout/footer/index.jsx
@@ -4,10 +4,12 @@ import { Paragraph } from '@typography'
 
 const index = () => {
     return (
-        <footer className='w-full px-10'>
+        <footer className='w-full pt-12 pb-10'>
             <div className='text-center'>
                 <Paragraph>This website is for competition purpose</Paragraph>
-                <Paragraph className='text-xs'>DataLabs Team</Paragraph>
+                <Paragraph className='text-xs' secondary>
+                    DataLabs Team
+                </Paragraph>
             </div>
         </footer>
     )
