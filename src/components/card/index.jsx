80060666b98a7355718804e6427774473c01e2be
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-22 16:09:08 +0700

    fix card layout

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -6,7 +6,7 @@ const card = ({ text1, text2, text3 }) => {
     return (
         <div className='border-2 border-gray-600 rounded'>
             <div className='w-full h-40 bg-gray-300'></div>
-            <div className='p-4 flex flex-row gap-2'>
+            <div className='p-4 flex flex-col gap-2'>
                 <Paragraph>{text1}</Paragraph>
                 <Paragraph>{text2}</Paragraph>
                 <Paragraph>{text3}</Paragraph>
