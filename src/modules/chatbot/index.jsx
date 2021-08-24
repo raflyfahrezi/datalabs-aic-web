6482be3c421243e20b3d846000778abfff99fbe7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 11:08:21 +0700

    implement send button

--- src/modules/chatbot/index.jsx
+++ src/modules/chatbot/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
+import { SendIcon } from '@assets'
 import { TextField, ComponentWrapper } from '@components'
 
 const chatbot = () => {
     return (
         <ComponentWrapper>
-            <div className='flex flex-col bg-gray-600'>
-                <div>halo</div>
-                <div>
+            <div className='w-full h-full grid grid-rows-pageWrapper '>
+                <div className='border-2 border-gray-800 rounded'>halo</div>
+                <div className='py-6 flex'>
                     <TextField
                         className='w-full'
                         placeholder='Enter a message'
                     />
+                    <div className='w-14 ml-2 grid place-items-center rounded bg-gray-800'>
+                        <SendIcon />
+                    </div>
                 </div>
             </div>
         </ComponentWrapper>
