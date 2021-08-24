869d7e4bf720c17ae0196521d99f4f5903c96597
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 10:40:39 +0700

    implement chatbot module

--- src/pages/chatbot.jsx
+++ src/pages/chatbot.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { Chatbot } from '@module'
 import { PageWrapper } from '@layout'
 
 const chatbot = () => {
-    return <PageWrapper title='Chatbot'></PageWrapper>
+    return (
+        <PageWrapper title='Chatbot'>
+            <Chatbot />
+        </PageWrapper>
+    )
 }
 
 export default chatbot
