c855eab4df8dbf432d4acfc7a5ac034c141d45aa
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 10:56:07 +0700

    remove some styles

--- src/modules/chatbot/index.jsx
+++ src/modules/chatbot/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { TextField, ComponentWrapper } from '@components'
+
 const chatbot = () => {
     return (
-        <div>
-            <div></div>
-        </div>
+        <ComponentWrapper>
+            <div className='flex flex-col bg-gray-600'>
+                <div>halo</div>
+                <div>
+                    <TextField
+                        className='w-full'
+                        placeholder='Enter a message'
+                    />
+                </div>
+            </div>
+        </ComponentWrapper>
     )
 }
 
