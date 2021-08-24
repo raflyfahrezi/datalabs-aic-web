29d3bee59a9bc57ac686063369eab746bdab2689
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 11:13:57 +0700

    create chatbox

---
+++ src/modules/chatbot/box/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const box = ({ chatBox }) => {
+    return (
+        <div className='w-full h-full'>
+            <div></div>
+        </div>
+    )
+}
+
+export default box
--- src/modules/chatbot/index.jsx
+++ src/modules/chatbot/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { SendIcon } from '@assets'
 import { TextField, ComponentWrapper } from '@components'
 
+import ChatBox from './box'
+
 const chatbot = () => {
+    const [chatHistory, setChatHistory] = useState([])
+
     return (
         <ComponentWrapper>
             <div className='w-full h-full grid grid-rows-pageWrapper '>
-                <div className='border-2 border-gray-800 rounded'>halo</div>
+                <div className='border-2 border-gray-800 rounded'>
+                    <ChatBox chatHistory={chatHistory} />
+                </div>
                 <div className='py-6 flex'>
                     <TextField
                         className='w-full'
