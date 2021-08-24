6dac30808f2b92b774a557fe5464ce20c7a8ca27
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 17:22:44 +0700

    fix get response from bot

--- src/modules/chatbot/index.jsx
+++ src/modules/chatbot/index.jsx
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import React, { useRef, useState, useEffect } from 'react'
 
 import { SendIcon } from '@assets'
 import { TextField, ComponentWrapper } from '@components'
 
 import ChatBox from './box'
+import { async } from 'regenerator-runtime'
 
 const chatbot = () => {
+    const isFirstRender = useRef(false)
+
     const [message, setMessage] = useState('')
-    const [isWaitingResponse, setIsWaitingResponse] = useState(false)
     const [chatHistory, setChatHistory] = useState([])
+    const [requestResponse, setRequestResponse] = useState(false)
+    const [isWaitingResponse, setIsWaitingResponse] = useState(false)
 
     const messageChangeHandler = (e) => {
         const { value } = e.target
@@ -16,14 +21,47 @@ const chatbot = () => {
         setMessage(value)
     }
 
+    const pushMessageToHistory = ({ type, message }) => {
+        const chat = { type, message }
+
+        setChatHistory([...chatHistory, { ...chat }])
+    }
+
     const sendMessageHandler = (e) => {
         e.preventDefault()
 
-        const chat = { type: 'user', message }
-
-        setChatHistory([...chatHistory, { ...chat }])
         setMessage('')
+        pushMessageToHistory({ type: 'user', message: message })
+        setRequestResponse(true)
     }
+
+    useEffect(async () => {
+        if (isFirstRender.current === false) {
+            isFirstRender.current = true
+
+            return
+        }
+
+        if (requestResponse === false) {
+            return
+        }
+
+        try {
+            const { data } = await axios.post(
+                'https://chatbot-datalabs.et.r.appspot.com',
+                { chat: message }
+            )
+
+            pushMessageToHistory({ type: 'bot', message: data.res })
+        } catch (e) {
+            pushMessageToHistory({
+                type: 'bot',
+                message: 'Oops! Something went wrong',
+            })
+        }
+
+        setRequestResponse(false)
+    }, [requestResponse])
 
     return (
         <ComponentWrapper>
