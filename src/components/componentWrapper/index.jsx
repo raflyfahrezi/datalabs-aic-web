a289c48563069482b91c2eb071e0bd24233c5990
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 17:30:53 +0700

    fix componentWrapper

--- src/components/componentWrapper/index.jsx
+++ src/components/componentWrapper/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const componentWrapper = ({ children }) => {
-    return (
-        <div className='w-full max-w-screen-lg h-full mx-auto px-2'>
-            {children}
-        </div>
-    )
+    return <div className='w-full max-w-screen-lg mx-auto px-2'>{children}</div>
 }
 
 export default componentWrapper
