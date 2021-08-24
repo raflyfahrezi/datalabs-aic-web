90dac5fc11a3286247e4ecd50d8d3062b81d2e7c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-24 11:39:34 +0700

    edit chat box component

--- src/modules/chatbot/box/index.jsx
+++ src/modules/chatbot/box/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-const box = ({ chatBox }) => {
+import { Chat } from '@components'
+
+const box = ({ chatHistory }) => {
     return (
         <div className='w-full h-full'>
-            <div></div>
+            {/* Greeting Messages */}
+            <div className='py-6'>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.5 }}
+                >
+                    <Chat type='greet'>
+                        Welcome to DataLabs Courses Chatbot. Ask us some
+                        question and we will reply as fast as we can.
+                    </Chat>
+                </motion.div>
+            </div>
+
+            {/* User and Bot Messages */}
+            <div>
+                {chatHistory.map((item, index) => {
+                    return (
+                        <Chat key={index} type='user'>
+                            {item.message}
+                        </Chat>
+                    )
+                })}
+            </div>
         </div>
     )
 }
