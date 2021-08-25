5720123c8af93f372674453432559aa63a167005
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-25 10:08:08 +0700

    fix card wrapper

--- src/components/cardWrapper/index.jsx
+++ src/components/cardWrapper/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import { ComponentWrapper } from '@components'
+
 const cardWrapper = ({ children }) => {
     return (
-        <div className='w-full grid grid-flow-col gap-4 overflow-auto'>
-            {children}
+        <div className='w-full overflow-auto'>
+            <ComponentWrapper>
+                <div className='w-min grid grid-flow-col gap-4 '>
+                    {children}
+                </div>
+            </ComponentWrapper>
         </div>
     )
 }
