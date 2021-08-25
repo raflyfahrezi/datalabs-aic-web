d515fd9fa375ba3fa852508ca4bdb271f7229aa2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-25 10:12:27 +0700

    edit cardWrapper

--- src/components/cardWrapper/index.jsx
+++ src/components/cardWrapper/index.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import { ComponentWrapper } from '@components'
 
-const cardWrapper = ({ children }) => {
+const cardWrapper = ({ className, children }) => {
     return (
-        <div className='w-full overflow-auto'>
+        <div
+            className={`w-full overflow-auto scrollbar-hide ${className ?? ''}`}
+        >
             <ComponentWrapper>
                 <div className='w-min grid grid-flow-col gap-4 '>
                     {children}
