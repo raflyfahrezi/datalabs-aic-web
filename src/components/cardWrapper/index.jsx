24088f6f62b3bd0efc16e82759ffed04b1b67bf9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-25 10:17:45 +0700

    edit cardWrapper

--- src/components/cardWrapper/index.jsx
+++ src/components/cardWrapper/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
+import { Heading2 } from '@typography'
 import { ComponentWrapper } from '@components'
 
-const cardWrapper = ({ className, children }) => {
+const cardWrapper = ({ title, className, children }) => {
     return (
-        <div
-            className={`w-full overflow-auto scrollbar-hide ${className ?? ''}`}
-        >
+        <div>
             <ComponentWrapper>
-                <div className='w-min grid grid-flow-col gap-4 '>
-                    {children}
-                </div>
+                <Heading2>{title}</Heading2>
             </ComponentWrapper>
+            <div
+                className={`w-full py-6 overflow-auto scrollbar-hide ${
+                    className ?? ''
+                }`}
+            >
+                <ComponentWrapper>
+                    <div className='w-min grid grid-flow-col gap-4 '>
+                        {children}
+                    </div>
+                </ComponentWrapper>
+            </div>
         </div>
     )
 }
