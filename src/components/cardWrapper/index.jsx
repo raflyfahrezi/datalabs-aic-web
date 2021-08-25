d28b85c0c45b8e4dec638d6cb7740ecbe27a5f86
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-25 11:13:59 +0700

    implement next and previous functionality

--- src/components/cardWrapper/index.jsx
+++ src/components/cardWrapper/index.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useRef, useEffect, Children } from 'react'
 
 import { Heading2 } from '@typography'
 import { ComponentWrapper } from '@components'
 import { NextIcon, PreviousIcon } from '@assets'
 
 const cardWrapper = ({ title, className, children }) => {
+    const cardWrapper = useRef()
+    const cardWrapperContent = useRef()
+
+    const getChildrenLength = Children.toArray(children).length
+
+    const getSizePerCard = () => {
+        const sizePerCard =
+            cardWrapperContent.current.scrollWidth / getChildrenLength
+
+        return sizePerCard
+    }
+
+    const getCurrentPosition = () => {
+        const currentPositions = cardWrapper.current.scrollLeft
+
+        return currentPositions
+    }
+
+    const previousOnClickHandler = () => {
+        cardWrapper.current.scrollLeft = getCurrentPosition() - getSizePerCard()
+    }
+
+    const nextOnClickHandler = () => {
+        cardWrapper.current.scrollLeft = getCurrentPosition() + getSizePerCard()
+    }
+
+    useEffect(() => {
+        if (cardWrapper.current) {
+            cardWrapper.current.scrollLeft = 0
+        }
+    }, [children])
+
     return (
         <div>
             <ComponentWrapper>
@@ -13,22 +45,32 @@ const cardWrapper = ({ title, className, children }) => {
                         <Heading2>{title}</Heading2>
                     </div>
                     <div>
-                        <button className='hover:bg-gray-200 transition-all duration-500'>
+                        <button
+                            onClick={previousOnClickHandler}
+                            className='hover:bg-gray-200 transition-all duration-500'
+                        >
                             <PreviousIcon />
                         </button>
-                        <button className='hover:bg-gray-200 transition-all duration-500'>
+                        <button
+                            onClick={nextOnClickHandler}
+                            className='hover:bg-gray-200 transition-all duration-500'
+                        >
                             <NextIcon />
                         </button>
                     </div>
                 </div>
             </ComponentWrapper>
             <div
-                className={`w-full py-6 overflow-auto scrollbar-hide ${
+                ref={cardWrapper}
+                className={`w-full py-6 overflow-auto scrollbar-hide  ${
                     className ?? ''
                 }`}
             >
                 <ComponentWrapper>
-                    <div className='w-min grid grid-flow-col gap-4 '>
+                    <div
+                        ref={cardWrapperContent}
+                        className='w-min grid grid-flow-col gap-4 '
+                    >
                         {children}
                     </div>
                 </ComponentWrapper>
