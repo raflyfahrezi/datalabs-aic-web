731b9ad3ab491c566dd03c1fa73e146e62a670a4
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-25 11:06:00 +0700

    adding next and previous button

--- src/components/cardWrapper/index.jsx
+++ src/components/cardWrapper/index.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 
 import { Heading2 } from '@typography'
 import { ComponentWrapper } from '@components'
+import { NextIcon, PreviousIcon } from '@assets'
 
 const cardWrapper = ({ title, className, children }) => {
     return (
         <div>
             <ComponentWrapper>
-                <Heading2>{title}</Heading2>
+                <div className='flex justify-between items-center'>
+                    <div>
+                        <Heading2>{title}</Heading2>
+                    </div>
+                    <div>
+                        <button className='hover:bg-gray-200 transition-all duration-500'>
+                            <PreviousIcon />
+                        </button>
+                        <button className='hover:bg-gray-200 transition-all duration-500'>
+                            <NextIcon />
+                        </button>
+                    </div>
+                </div>
             </ComponentWrapper>
             <div
                 className={`w-full py-6 overflow-auto scrollbar-hide ${
