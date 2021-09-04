aab9fdf507b402bfc8b2cd87e2e2098a2a6e9a48
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-09-04 20:44:37 +0700

    implement getBotResponse

--- src/utils/getBotResponse/index.js
+++ src/utils/getBotResponse/index.js
@@ -1,3 +1,19 @@
-const getBotResponse = ({}) => {}
+import axios from 'axios'
+
+const getBotResponse = async ({ chatHistory, pushMessageToHistory }) => {
+    const body = new URLSearchParams()
+
+    const copyOfChatHistory = [...chatHistory]
+    const userMessage = copyOfChatHistory.pop()
+
+    body.append('chat', userMessage.message)
+
+    const { data } = await axios.post(
+        'https://chatbot-datalabs.et.r.appspot.com/',
+        body
+    )
+
+    pushMessageToHistory({ type: 'bot', message: data.res })
+}
 
 export default getBotResponse
